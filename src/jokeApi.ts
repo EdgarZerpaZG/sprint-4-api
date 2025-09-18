7a89b2ed60203631b3218a0db82242bd52c4d273
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-09-18 10:14:54 +0200

    Added jokes

--- src/jokeApi.ts
+++ src/jokeApi.ts
@@ -1,5 +1,24 @@
-import dotenv from "dotenv";
+import type { JokeResponse } from "./types";
 
-dotenv.config();
+const JOKE_URL = import.meta.env.VITE_CALLBACK_RANDOM_JOKE;
 
-const JOKE_URL = process.env.CALLBACK_RANDOM_JOKE;
+
+export async function getRandomJoke(): Promise<JokeResponse | null> {
+  try {
+    const response = await fetch(JOKE_URL, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error en la petición: ${response.status}`);
+    }
+
+    const data: JokeResponse = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al obtener la información:", error);
+    return null;
+  }
+}
--- src/jokeMain.ts
+++ src/jokeMain.ts
@@ -1,1 +1,42 @@
 import './style.css';
+import { getRandomJoke } from "./jokeApi";
+
+let jokesHistory: string[] = [];
+
+function loadHistory() {
+  const savedJokes = localStorage.getItem("jokesHistory");
+  if (savedJokes) {
+    jokesHistory = JSON.parse(savedJokes);
+    console.log("Historial cargado:", jokesHistory);
+  }
+}
+
+function saveHistory() {
+  localStorage.setItem("jokesHistory", JSON.stringify(jokesHistory));
+}
+
+async function showJoke() {
+  const jokeData = await getRandomJoke();
+  const jokeElement = document.getElementById("joke");
+
+  if (jokeData && jokeElement) {
+    jokeElement.textContent = `🤣 ${jokeData.joke}`;
+
+    jokesHistory.push(jokeData.joke);
+
+    saveHistory();
+
+    console.log("Historial de chistes:", jokesHistory);
+  } else {
+    if (jokeElement) {
+      jokeElement.textContent = "No se pudo obtener un chiste.";
+    }
+  }
+}
+
+loadHistory();
+
+const generateJokeBtn = document.getElementById("generate-joke");
+if (generateJokeBtn) {
+  generateJokeBtn.addEventListener("click", showJoke);
+}
